e457cee9279f329ea256dc75f09dfa233ed2f419
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-08-23 09:41:44 +0700

    [andri/eko] add modal create new alert

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -34,14 +34,14 @@ class AlertsController < ApplicationController
   # POST /alerts.json
   def create
     receivers = params['receiver']
-    callback =  Hash.new
+    callback = Hash.new
     callback["type"] = params['callback']
     callback["receivers"] = receivers
 
     @alert = Alert.new()
     @alert.app_name = params['app_name'].downcase
     @alert.log_level = params['log_level'].downcase
-    @alert.duration = (params['hour'].to_i * 3600) + (params['minute'].to_i * 60) + params['second'].to_i
+    @alert.duration = params['duration']
     @alert.limit = params['limit']
     @alert.callback = callback.to_json
 
@@ -55,7 +55,7 @@ class AlertsController < ApplicationController
     @alert = Alert.find(params[:id])
 
     receivers = params['receiver']
-    callback =  Hash.new
+    callback = Hash.new
     callback["type"] = params['callback']
     callback["receivers"] = receivers
 
@@ -74,19 +74,20 @@ class AlertsController < ApplicationController
   def destroy
     @alert.destroy
     respond_to do |format|
-      format.html { redirect_to alerts_url, notice: 'Alert was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to alerts_url, notice: 'Alert was successfully destroyed.'}
+      format.json {head :no_content}
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_alert
-      @alert = Alert.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def alert_params
-      params.require(:alert).permit(:app_name, :log_level, :duration, :limit, :callback)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_alert
+    @alert = Alert.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def alert_params
+    params.require(:alert).permit(:app_name, :log_level, :duration, :limit, :callback)
+  end
 end
