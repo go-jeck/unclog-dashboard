467746124243bcdd3ca5cdc22575cadfc06c9bef
Eko Simanjuntak <eko.fikranta@aux.go-jek.com>
2018-08-28 15:56:35 +0700

    Finish to show chart for specific app name, node id, and log level

--- app/controllers/prediction_controller.rb
+++ app/controllers/prediction_controller.rb
@@ -2,70 +2,7 @@ class PredictionController < ApplicationController
   helper_method :options_prediction, :node_id, :log_level
 
   def log_metric_prediction
-    x_axis = LogMetricPrediction.distinct.pluck(:timestamp)
-    labels = LogMetricPrediction.distinct.pluck(:log_level)
 
-    datasets = []
-
-    labels.each do |label|
-      color = random_color
-      quantities = LogMetricPrediction.select('quantity').where(log_level: label).pluck('quantity')
-      items = []
-
-      if quantities.length < x_axis.length then
-        diff = x_axis.length - quantities.length
-
-        for i in 0...diff
-          items.push(0)
-        end
-      end
-
-      items.concat(quantities)
-
-      datasets.push(
-        label: label,
-        borderColor: color,
-        backgroundColor: color,
-        data: items,
-        fill: false
-      )
-
-      Rails.logger.debug(items)
-    end
-
-    @data = {
-      labels: x_axis,
-      datasets: datasets
-    }
-
-    @options = {
-      responsive: true,
-      maintainAspectRatio: false,
-      tooltips: {
-        mode: 'index',
-        intersect: false
-      },
-      hover: {
-        mode: 'nearest',
-        intersect: true
-      },
-      scales: {
-        xAxes: [{
-          display: false,
-          scaleLabel: {
-            display: true,
-            labelString: 'Time'
-          }
-        }],
-        yAxes: [{
-          display: true,
-          scaleLabel: {
-            display: true,
-            labelString: 'Quantity'
-          }
-        }]
-      }
-    }
   end
 
   def app_options
@@ -74,7 +11,9 @@ class PredictionController < ApplicationController
   end
 
   def chart_data
+    logger.info(params)
     data = LogMetricPrediction.select('*').where(:app_name => params['app_name'], :node_id => params['node_id'], :log_level => params['log_level'])
+    logger.info(data)
     render json: data
   end
 
